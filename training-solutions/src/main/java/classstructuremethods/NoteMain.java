2357dcf488cc82f6e06f4a73ec70101890587068
K4t1 <kata.percze@gmail.com>
2021-10-17 21:44:37 +0200

    Note and NoteMain done

---
+++ training-solutions/src/main/java/classstructuremethods/NoteMain.java
@@ -0,0 +1,20 @@
+package classstructuremethods;
+
+import java.util.Scanner;
+
+public class NoteMain {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Note note = new Note();
+
+        System.out.println("What is your name?");
+        note.setName(scanner.nextLine());
+        System.out.println("What is your topic?");
+        note.setTopic(scanner.nextLine());
+        System.out.println("What is your text?");
+        note.setText(scanner.nextLine());
+
+        System.out.println(note.getNoteText());
+    }
+}
