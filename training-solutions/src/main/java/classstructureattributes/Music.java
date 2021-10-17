785e38d4f8add65f8fb97ac77d45ee6b33049ded
K4t1 <kata.percze@gmail.com>
2021-10-17 20:37:45 +0200

    Song and Music done

--- training-solutions/src/main/java/classstructureattributes/Music.java
+++ training-solutions/src/main/java/classstructureattributes/Music.java
@@ -3,6 +3,7 @@ package classstructureattributes;
 import java.util.Scanner;
 
 public class Music {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Song favourite = new Song();
