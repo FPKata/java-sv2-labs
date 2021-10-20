56bff4e485ba63a74ca04cece938180177bc20c6
K4t1 <kata.percze@gmail.com>
2021-10-20 13:14:42 +0200

    Store and StoreMain done

---
+++ training-solutions/src/main/java/classstructureconstructors/StoreMain.java
@@ -0,0 +1,22 @@
+package classstructureconstructors;
+
+public class StoreMain {
+    public static void main(String[] args) {
+
+        Store store1 = new Store("coffee");
+        System.out.println("Product1 :" + store1.getProduct() + " " + store1.getStock() + " " + "ton(s)");
+
+        Store store2 = new Store("banana");
+        System.out.println("Product2 :" + store2.getProduct() + " " + store2.getStock() + " " + "ton(s)");
+
+        store1.store(11);
+        store2.store(22);
+        System.out.println("Product1 :" + store1.getProduct() + " " + store1.getStock() + " " + "ton(s)");
+        System.out.println("Product2 :" + store1.getProduct() + " " + store2.getStock() + " " + "ton(s)");
+
+        store1.dispatch(1);
+        store2.dispatch(2);
+        System.out.println("Product1 :" + store1.getProduct() + " " + store1.getStock() + " " + "ton(s)");
+        System.out.println("Product2 :" + store1.getProduct() + " " + store2.getStock() + " " + "ton(s)");
+    }
+}
