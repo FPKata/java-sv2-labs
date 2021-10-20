1fc0deefc01f3f412349da5d882402a4ddfd7c8c
K4t1 <kata.percze@gmail.com>
2021-10-20 13:19:27 +0200

    StoreMain error corrected

--- training-solutions/src/main/java/classstructureconstructors/StoreMain.java
+++ training-solutions/src/main/java/classstructureconstructors/StoreMain.java
@@ -12,11 +12,11 @@ public class StoreMain {
         store1.store(11);
         store2.store(22);
         System.out.println("Product1 :" + store1.getProduct() + " " + store1.getStock() + " " + "ton(s)");
-        System.out.println("Product2 :" + store1.getProduct() + " " + store2.getStock() + " " + "ton(s)");
+        System.out.println("Product2 :" + store2.getProduct() + " " + store2.getStock() + " " + "ton(s)");
 
         store1.dispatch(1);
         store2.dispatch(2);
         System.out.println("Product1 :" + store1.getProduct() + " " + store1.getStock() + " " + "ton(s)");
-        System.out.println("Product2 :" + store1.getProduct() + " " + store2.getStock() + " " + "ton(s)");
+        System.out.println("Product2 :" + store2.getProduct() + " " + store2.getStock() + " " + "ton(s)");
     }
 }
