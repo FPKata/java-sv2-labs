d59213aa2aca294e7099eda800b09f70aebf680b
FPKata <kata.percze@gmail.com>
2021-11-01 11:11:36 +0100

    Heading North done

---
+++ training-solutions/src/main/java/operators/HeadingNorth.java
@@ -0,0 +1,17 @@
+/*package operators;
+
+public class HeadingNorth {
+    public int getNumberOfNewDirection(){
+        int angle = 0;
+        int number = 1;
+        while (angle != 360){
+            angle = angle + 10 * number;
+            number++;
+        }
+        return number;
+    }
+    public static void main(String[] args) {
+        HeadingNorth headingNorth = new HeadingNorth();
+        System.out.println(headingNorth.getNumberOfNewDirection());
+    }
+}*/
