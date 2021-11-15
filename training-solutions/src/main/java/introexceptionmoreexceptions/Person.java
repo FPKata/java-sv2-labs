329ab5b96c9b8b15ab4a9fd0cf23528dc1c80252
FPKata <kata.percze@gmail.com>
2021-11-15 14:19:21 +0100

    Person done

---
+++ training-solutions/src/main/java/introexceptionmoreexceptions/Person.java
@@ -0,0 +1,33 @@
+package introexceptionmoreexceptions;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person:" +
+                " (name) " + name +
+                " (age) " + age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person(null, 32);
+        System.out.println(person);
+        System.out.println(person.getName());
+        System.out.println(person.getName().toUpperCase());
+    }
+}
