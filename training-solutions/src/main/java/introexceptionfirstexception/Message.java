c81c3830296e7e7ad6fffc62947251233cc5f890
FPKata <kata.percze@gmail.com>
2021-11-15 12:27:01 +0100

    Message done

---
+++ training-solutions/src/main/java/introexceptionfirstexception/Message.java
@@ -0,0 +1,25 @@
+package introexceptionfirstexception;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Message {
+    public static void main(String[] args) {
+        List<String> codedMessage = Arrays.asList("76", "101", "103", "121", "101", "110", "32", "115", "122",
+                "233", "112", "32", "110", "97", "112", "111", "100", "33");
+
+        for (String actual : codedMessage) {
+            char realMessage = (char) Integer.parseInt(actual);
+            System.out.print(realMessage);
+        }
+        System.out.println();
+        List<String> anotherCodedMessage = Arrays.asList("69", "122", "32", "101", "103", "121", "32", "104", "105",
+                "98", "225", "115", "32", "252", "122", "101", "110", "101", "116", "46");
+
+        for (String actual : anotherCodedMessage) {
+            char anotherRealMessage = (char) Integer.parseInt(actual);
+            System.out.print(anotherRealMessage);
+        }
+    }
+}
