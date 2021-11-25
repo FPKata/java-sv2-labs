b3be915dc03bfa891ccb7189a4bdd5881c74ca51
FPKata <kata.percze@gmail.com>
2021-11-25 19:30:34 +0100

    Registration corrected

--- training-solutions/src/main/java/introexceptionthrow/Registration.java
+++ training-solutions/src/main/java/introexceptionthrow/Registration.java
@@ -6,12 +6,14 @@ public class Registration {
     public static void main(String[] args) {
         Validation validation = new Validation();
         Scanner scanner = new Scanner(System.in);
+        boolean validReg = true;
 
         System.out.println("Welcome our page! \nPlease enter your name:");
         String name = scanner.nextLine();
         try {
             validation.validateName(name);
         }catch (IllegalArgumentException iae){
+            validReg = false;
             System.out.println(iae.getMessage());
         }
 
@@ -20,11 +22,18 @@ public class Registration {
         try {
             validation.validateAge(age);
         }catch (IllegalArgumentException iae){
+            validReg = false;
             System.out.println(iae.getMessage());
         }
 
         System.out.println("Your name is: " + name);
         System.out.println("Your age is: " + age);
+
+        if (validReg){
+            System.out.println("Your registration was successful.");
+        }else{
+            System.out.println("Your registration was not successful.");
+        }
 
     }
 }
