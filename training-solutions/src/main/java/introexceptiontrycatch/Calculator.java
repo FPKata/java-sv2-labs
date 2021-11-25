108d24c6e1102d5fedabeb85c2a543f64535c261
FPKata <kata.percze@gmail.com>
2021-11-25 15:09:48 +0100

    Calculator corrected

--- training-solutions/src/main/java/introexceptiontrycatch/Calculator.java
+++ training-solutions/src/main/java/introexceptiontrycatch/Calculator.java
@@ -6,34 +6,51 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Kérem az első számot:");
-        int firstOperand = scanner.nextInt();
-        scanner.nextLine();
+        String firstOperand = scanner.nextLine();
+        int number1 = 0;
+
+        try{
+            number1 = Integer.parseInt(firstOperand);
+        }catch (NumberFormatException nfe){
+            System.out.println("Nem szám!");
+            System.out.println("Alapértelmezettként 0 lesz.");
+        }
+
         System.out.println("Kérem a második számot:");
-        int secondOperand = scanner.nextInt();
-        scanner.nextLine();
+        String secondOperand = scanner.nextLine();
+        int number2 = 0;
+
+        try{
+            number2 = Integer.parseInt(secondOperand);
+        }catch (NumberFormatException nfe){
+            System.out.println("Nem szám!");
+            System.out.println("Alapértelmezettként 0 lesz");
+        }
+
         System.out.println("Milyen műveletet végezzek el rajtuk: (+, -, *, /)");
         String operator = scanner.nextLine();
 
-        try{
-            switch(operator){
-                case "+":
-                    System.out.println(firstOperand + secondOperand);
-                    break;
-                case "-":
-                    System.out.println(firstOperand - secondOperand);
-                    break;
-                case "*":
-                    System.out.println(firstOperand * secondOperand);
-                    break;
-                case "/":
-                    System.out.println(firstOperand / secondOperand);
-                    break;
-                default:
-                    System.out.println("Nincs ilyen művelet...");
+        switch(operator){
+            case "+":
+                System.out.println(number1 + number2);
+            break;
+            case "-":
+                System.out.println(number1 - number2);
+            break;
+            case "*":
+                System.out.println(number1 * number2);
+                break;
+            case "/":
+            try{
+                System.out.println(number1 / number2);
+                break;
             }
-        }
-        catch(ArithmeticException ae){
-            System.out.println("A 0-val való osztás értelmetlen");
+            catch(ArithmeticException ae){
+                System.out.println("A 0-val való osztás értelmetlen");
+                break;
+            }
+            default:
+                System.out.println("Nincs ilyen művelet...");
         }
     }
 }
