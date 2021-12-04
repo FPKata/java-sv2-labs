1f82037c8dd7a3e590953338758092b12d9b03e6
FPKata <kata.percze@gmail.com>
2021-12-04 17:47:48 +0100

    TwoLetters/Test done

---
+++ training-solutions/src/test/java/algorithmstransformation/letters/TwoLettersTest.java
@@ -0,0 +1,20 @@
+package algorithmstransformation.letters;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoLettersTest {
+    TwoLetters twoLetters = new TwoLetters();
+
+    @Test
+    void testGetFirstTwoLetters(){
+        List<String> words = Arrays.asList("kocsi", "mamusz", "bajusz", "óra", "tehén");
+
+        assertEquals(Arrays.asList("ko", "ma", "ba", "ór", "te"), twoLetters.getFirstTwoLetters(words));
+    }
+
+}
