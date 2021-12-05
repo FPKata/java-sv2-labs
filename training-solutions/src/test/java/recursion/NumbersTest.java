158e661acaeafb4a1fb00e64cc1a8edea280326d
FPKata <kata.percze@gmail.com>
2021-12-05 08:06:42 +0100

    Numbers/Test done

---
+++ training-solutions/src/test/java/recursion/NumbersTest.java
@@ -0,0 +1,16 @@
+package recursion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumbersTest {
+    @Test
+    void testGetSum(){
+        Numbers numbers = new Numbers();
+
+        //assertEquals(10, numbers.getSum(new int[] {1, 2, 3, 4}));
+        assertEquals(4, numbers.getSum(new int[] {4}));
+        assertEquals(0, numbers.getSum(new int[] {}));
+    }
+}
