c943673e7cbe8679466eda5a65b8e8a97aec1675
FPKata <kata.percze@gmail.com>
2022-01-17 21:36:42 +0100

    Grammar done

--- training-solutions/src/main/java/methodstructure/bmi/BodyMass.java
+++ training-solutions/src/main/java/methodstructure/bmi/BodyMass.java
@@ -1,39 +1,40 @@
-package methodstructure.bmi;
-
-public class BodyMass {
-    private double weight;
-    private double height;
-
-    public BodyMass(double weight, double height) {
-        this.weight = weight;
-        this.height = height;
-    }
-     public double getBodyMassIndex(){
-         return weight / (height * height);
-     }
-
-     public BmiCategory getBody(){
-        BmiCategory category;
-        double bmi = getBodyMassIndex();
-        if (bmi < 18.5){
-            category = BmiCategory.UNDERWEIGHT;
-        }else if (bmi <25){
-            category = BmiCategory.NORMAL;
-        }else {
-            category = BmiCategory.OVERWEIGHT;
-        }
-        return category;
-     }
-
-     public boolean isThinnerThan(BodyMass other){
-        return getBodyMassIndex() < other.getBodyMassIndex();
-     }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-}
+//package methodstructure.bmi;
+//
+//public class BodyMass {
+//    private double weight;
+//    private double height;
+//
+//    public BodyMass(double weight, double height) {
+//        this.weight = weight;
+//        this.height = height;
+//    }
+//     public double getBodyMassIndex(){
+//         return weight / (height * height);
+//     }
+//
+//     public BmiCategory getBody(){
+//        BmiCategory category;
+//        double bmi = getBodyMassIndex();
+//        if (bmi < 18.5){
+//            category = BmiCategory.UNDERWEIGHT;
+//        }else if (bmi <25){
+//            category = BmiCategory.NORMAL;
+//        }else {
+//            category = BmiCategory.OVERWEIGHT;
+//        }
+//        return category;
+//     }
+//
+//     public boolean isThinnerThan(BodyMass other){
+//        return getBodyMassIndex() < other.getBodyMassIndex();
+//     }
+//
+//    public double getWeight() {
+//        return weight;
+//    }
+//
+//    public double getHeight() {
+//        return height;
+//    }
+//
+//}
