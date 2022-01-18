92aeaab41a9b6a23f8050898da3db73b8e503a4b
FPKata <kata.percze@gmail.com>
2022-01-18 20:18:59 +0100

    Aid done

---
+++ training-solutions/src/main/java/exceptionmulticatch/aid/Aid.java
@@ -0,0 +1,36 @@
+package exceptionmulticatch.aid;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aid {
+    private int amount;
+
+    public Aid(int amount) {
+        this.amount = amount;
+    }
+
+    public List<String> countAmountsOfAid(Path path){
+        List<String> result = new ArrayList<>();
+        try{
+            List<String> numberOfPeople = Files.readAllLines(path);
+            for (String actual : numberOfPeople){
+                String[] temp = actual.split(" ");
+                int people = Integer.parseInt(temp[1]);
+                int amountForOnePerson = amount / people;
+                result.add(temp[0] + " " + amountForOnePerson);
+            }
+        }catch(NullPointerException | IOException | IndexOutOfBoundsException | NumberFormatException |
+                ArithmeticException e){
+            throw new IllegalStateException("Something went wrong while counting.", e);
+        }
+        return result;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
